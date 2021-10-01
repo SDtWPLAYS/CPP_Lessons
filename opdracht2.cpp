133697846479b06a910a57cee707121a06fe599d
SDtWPLAYS <stijn.driessen4@gmail.com>
2021-10-01 14:31:38 +0200

    Added basics to opdracht2 and opdracht3

--- opdracht2.cpp
+++ opdracht2.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+
+namespace st = std;
+
+struct Vec3D{
+    float x, y, z;
+    Vec3D(float x, float y, float z);
+    void show(st::string label, float scalar);
+    void show(){
+        st::cout << '\n';
+    };
+    Vec3D minus(Vec3D const &self);
+    Vec3D add(Vec3D const &self, Vec3D const &other);
+}
--- opdracht3.cpp
+++ opdracht3.cpp
@@ -1,3 +1,21 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+
+namespace st = std;
+
+class Vec3D{
+    float x, y, z;
+    Vec3D(float x, float y, float z);
+    void show(st::string label, Vec3D const &self);
+    void show(st::string label, float scalar);
+    void show(){
+        st::cout << '\n';
+    };
+    Vec3D minus(Vec3D const &self);
+    Vec3D add(Vec3D const &self, Vec3D const &other);
+}
+
+class Ray{
+
+}
