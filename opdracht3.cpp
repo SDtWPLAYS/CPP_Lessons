a1fcbdf66f9b2cf14cb42f10a300f1b49e544150
SDtWPLAYS <stijn.driessen4@gmail.com>
2021-10-08 14:30:24 +0200

    opdracht 2 and opdracht 3 starting to initialize the functions. Not everything works just yet!

--- opdracht3.cpp
+++ opdracht3.cpp
@@ -6,21 +6,70 @@ namespace st = std;
 
 class Vec3D{
     float x, y, z;
-    Vec3D(float x, float y, float z);
-    void show(st::string label, Vec3D const &self);
-    void show(st::string label, float scalar);
+    Vec3D(float x, float y, float z){
+        this.x = x;
+        this.y is y;
+        this.z = z;
+    };
+    void show(st::string label, Vec3D const &self){
+        st::cout << label << " is op de coordinaten: x = " << self.x << " y = " << self.y << " z = " << self.z << ".\n";
+    };
+    void show(st::string label, float scalar){
+        st::cout << label << " is de scalar: " << scalar << ".\n";
+    };
     void show(){
         st::cout << '\n';
     };
-    Vec3D minus(Vec3D const &self);
-    Vec3D add(Vec3D const &self, Vec3D const &other);
-    Vec3D sub(Vec3D const &self, Vec3D const &other);
-    Vec3D mul(Vec3D const &self, float scalar);
-    Vec3D div(Vec3D const &self, float scalar);
-    float norm(Vec3D const &self);
-    Vec3D unit(Vec3D const &self);
-    float dot(Vec3D const &self, Vec3D const &other);
-    Vec3D cross(Vec3D const &self, Vec3D const &other);
+    Vec3D minus(Vec3D const &self){
+        auto x = -self.x;
+        auto y = -self.y;
+        auto z = -self.z;
+        return Vec3D(x,y,z);
+    };
+    Vec3D add(Vec3D const &self, Vec3D const &other){
+        auto x = self.x + other.x;
+        auto y = self.y + other.y;
+        auto z = self.z + other.z;
+        return Vec3D(x,y,z);
+    };
+    Vec3D sub(Vec3D const &self, Vec3D const &other){
+        auto x = self.x - other.x;
+        auto y = self.y - other.y;
+        auto z = self.z - other.z;
+        return Vec3D(x,y,z);
+    };
+    Vec3D mul(Vec3D const &self, float scalar){
+        auto x = self.x * scalar;
+        auto y = self.y * scalar;
+        auto z = self.z * scalar;
+        return Vec3D(x,y,z);
+    };
+    Vec3D div(Vec3D const &self, float scalar){
+        auto x = self.x / scalar;
+        auto y = self.y / scalar;
+        auto z = self.z / scalar;
+        return Vec3D(x,y,z);
+    };
+    float norm(Vec3D const &self){
+        auto length = pow(pow(self.x,2) + pow(self.y,2) + pow(self.z,2), 0.5);
+        return length;
+    };
+    Vec3D unit(Vec3D const &self){
+        auto scalar = norm(self);
+        return div(self, scalar);
+    };
+    float dot(Vec3D const &self, Vec3D const &other){
+        auto x = self.x * other.x;
+        auto y = self.y * other.y;
+        auto z = self.z * other.z;
+        return x + y + z;
+    };
+    Vec3D cross(Vec3D const &self, Vec3D const &other){
+        auto x = self.y * other.z - self.z * other.y;
+        auto y = self.z * other.x - self.x * other.z;
+        auto z = self.x * other.y - self.y * other.x;
+        return Vec3D(x,y,z);
+    };
 }
 
 int main(){
